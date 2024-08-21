7c27ed855cc7f3b2bccad09ffb6d6b41123b1707
Micael Hernandes <micaelmacedo100@outlook.com>
2024-08-20 21:41:33 -0300

    wip: validando tokens

--- back/routes/api.php
+++ back/routes/api.php
@@ -3,6 +3,19 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function(){
+    $user = \App\Models\User::create([
+       'name' => 'Teste',
+       'email' => 'teste1@teste.com',
+       'password' => bcrypt('123456')
+    ]);
+
+    $token = $user->createToken('token')->accessToken;
+
+    return response()->json(['token' => $token], 200);
+
+});
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
