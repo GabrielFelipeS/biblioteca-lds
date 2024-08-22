d7dd3a5b3a4553f35e026e79c578808406083338
Micael Hernandes <micaelmacedo100@outlook.com>
2024-08-22 19:58:48 -0300

    feat: rota de registro

--- back/routes/api.php
+++ back/routes/api.php
@@ -1,21 +1,9 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function(){
-    $user = \App\Models\User::create([
-       'name' => 'Teste',
-       'email' => 'teste1@teste.com',
-       'password' => bcrypt('123456')
-    ]);
-
-    $token = $user->createToken('token')->accessToken;
-
-    return response()->json(['token' => $token], 200);
-
+Route::middleware('guest')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
 });
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
