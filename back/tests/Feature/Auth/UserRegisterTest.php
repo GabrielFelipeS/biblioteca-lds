16e0fc97249580f07d1adf4b27309a22e3e5bdc2
Micael Hernandes <micaelmacedo100@outlook.com>
2024-08-22 19:39:35 -0300

    test: teste de registro inicial

---
+++ back/tests/Feature/Auth/UserRegisterTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserRegisterTest extends TestCase
+{
+
+    use WithFaker;
+
+    public function test_de_registro_com_dados_validos(){
+        $data = [
+            'name' => $this->faker->name,
+            'email' => $this->faker->email,
+            'password' => '123456',
+            'password_confirmation' => '123456'
+        ];
+
+        $response = $this->postJson('/api/register', $data);
+
+        $response->assertStatus(201);
+
+        $response->assertJsonStructure([
+            'token'
+        ]);
+    }
+}
