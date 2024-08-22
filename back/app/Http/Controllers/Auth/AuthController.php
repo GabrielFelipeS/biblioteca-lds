9992ae4240dcbd581292dcc5242a7d4d273391a1
Micael Hernandes <micaelmacedo100@outlook.com>
2024-08-22 20:25:52 -0300

    refactor: usando form request e tratando erros

--- back/app/Http/Controllers/Auth/AuthController.php
+++ back/app/Http/Controllers/Auth/AuthController.php
@@ -3,28 +3,21 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\UserRegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function register(Request $request){
-
-        $request->validate([
-            'name' => 'required|string',
-            'email' => 'email|unique:users|required',
-            'password' => 'required|string|min:6'
-        ]);
-
-        $data = [
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => $request->password
-        ];
-
-        $user = User::create($data);
-
-        return response()->json(['response' => 'Usuario criado com sucesso!'],201);
-
+    public function register(UserRegisterRequest $request)
+    {
+        try {
+            $data = $request->validated();
+            $data['password'] = bcrypt($data['password']);
+            $user = User::create($data);
+            return response()->json(['message' => 'Usuario registrado com sucesso!'], 201);
+        } catch (\Throwable $th) {
+            return response()->json(['message' => 'Erro ao registrar usuario!'], 500);
+        }
     }
 }
