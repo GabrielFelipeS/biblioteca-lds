5d86626f5f9bab09bb894055e70f6135fa44f97a
Micael Hernandes <micaelmacedo100@outlook.com>
2024-08-23 19:34:02 -0300

    feat: adicionando prefix as rotas de auth

--- back/routes/api.php
+++ back/routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('guest')->group(function () {
+Route::prefix('auth')->middleware('guest')->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
 });
