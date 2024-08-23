54a1557203e7b6e9c709c96943f96ad8559fcc1b
Micael Hernandes <micaelmacedo100@outlook.com>
2024-08-23 19:36:37 -0300

    feat: ajustes de rotas e nova rota logout

--- back/routes/api.php
+++ back/routes/api.php
@@ -4,7 +4,12 @@ use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('auth')->middleware('guest')->group(function () {
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/login', [AuthController::class, 'login']);
+Route::prefix('auth')->group(function () {
+    Route::middleware('guest')->group(function () {
+        Route::post('/register', [AuthController::class, 'register']);
+        Route::post('/login', [AuthController::class, 'login']);
+    });
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/logout', [AuthController::class, 'logout']);
+    });
 });
