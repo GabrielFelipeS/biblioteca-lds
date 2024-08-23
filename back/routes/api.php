48f1f99b11a19b55a2a1c81d6f8b7c278408906e
Micael Hernandes <micaelmacedo100@outlook.com>
2024-08-22 21:30:36 -0300

    feat: rota de login api

--- back/routes/api.php
+++ back/routes/api.php
@@ -6,4 +6,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
 });
