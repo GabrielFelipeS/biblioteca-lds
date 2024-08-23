e6ae9f3f716bc3bf63b582a36b14bb26bbf680e5
Micael Hernandes <micaelmacedo100@outlook.com>
2024-08-22 23:50:52 -0300

    refactor: ajustando metodo de login para usar a camada de servico

--- back/app/Http/Controllers/Auth/AuthController.php
+++ back/app/Http/Controllers/Auth/AuthController.php
@@ -28,17 +28,8 @@ class AuthController extends Controller
     public function login(UserLoginRequest $request)
     {
         try {
-            Log::info('Recebida requisição de login de usuario para o email: ' . $request->email);
-            $user = User::where('email', $request->email)->first();
-            if (!$user || !password_verify($request->password, $user->password)) {
-                Log::info('Credenciais inválidas para o email: ' . $request->email);
-                return response()->json(['message' => 'Credenciais inválidas!'], 401);
-            }
-            Log::info('Usuario logado com sucesso: ' . $request->email);
-            Log::info('Gerando token de acesso para o email: ' . $request->email);
-            $token = $user->createToken($user->email)->accessToken;
-            Log::info('Token de acesso gerado com sucesso para o email: ' . $request->email);
-            return response()->json(['token' => $token], 200);
+            $token = $this->service->login($request->validated());
+            return response()->json(['token' => $token]);
         } catch (\Throwable $th) {
             Log::error('Erro ao realizar login: ' . $th->getMessage());
             return response()->json(['message' => 'Erro ao realizar login!'], 500);
