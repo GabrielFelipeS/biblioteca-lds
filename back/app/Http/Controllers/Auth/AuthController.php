f90db13791e457ffec74f7e14793cf6cb8d4bb1c
Micael Hernandes <micaelmacedo100@outlook.com>
2024-08-22 23:39:57 -0300

    fix: ajustando nome da pasta repositories

--- back/app/Http/Controllers/Auth/AuthController.php
+++ back/app/Http/Controllers/Auth/AuthController.php
@@ -17,13 +17,8 @@ class AuthController extends Controller
     public function register(UserRegisterRequest $request)
     {
         try {
-            Log::info('Recebida requisição de registro de usuario para o email: ' . $request->email);
-            $data = $request->validated();
-            $data['password'] = bcrypt($data['password']);
-            Log::info('Registrando usuario: ' . $request->email);
-            $user = User::create($data);
-            Log::info('Usuario registrado com sucesso: ' . $request->email);
-            return response()->json(['message' => 'Usuario registrado com sucesso!'], 201);
+            $token = $this->service->register($request->validated());
+            return response()->json(['token' => $token], 201);
         } catch (\Throwable $th) {
             Log::error('Erro ao registrar usuario: ' . $th->getMessage());
             return response()->json(['message' => 'Erro ao registrar usuario!'], 500);
