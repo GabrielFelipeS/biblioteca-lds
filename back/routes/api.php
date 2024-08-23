9c7da745e136cd05d2015e7398bf0056d718c230
Micael Hernandes <micaelmacedo100@outlook.com>
2024-08-23 19:41:23 -0300

    fix: auterando middleware para o do passport rotas auth

--- back/routes/api.php
+++ back/routes/api.php
@@ -9,7 +9,7 @@ Route::prefix('auth')->group(function () {
         Route::post('/register', [AuthController::class, 'register']);
         Route::post('/login', [AuthController::class, 'login']);
     });
-    Route::middleware('auth:sanctum')->group(function () {
+    Route::middleware('auth:api')->group(function () {
         Route::post('/logout', [AuthController::class, 'logout']);
     });
 });
