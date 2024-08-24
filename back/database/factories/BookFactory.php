ea61c216c3f3e7d1eb5f09b9af0900244bf7ea6b
Yuri N Bertulucci <yuribertulucci2004@gmail.com>
2024-08-24 17:02:54 -0300

    refactor: change Book related Classes to singular

--- back/database/factories/BookFactory.php
+++ back/database/factories/BookFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Books>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
  */
-class BooksFactory extends Factory
+class BookFactory extends Factory
 {
     /**
      * Define the model's default state.
