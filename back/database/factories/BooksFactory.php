9cbcfd19ebcb5ca182dad29f0743a57344932203
Micael Hernandes <micaelmacedo100@outlook.com>
2024-08-24 01:15:11 -0300

    feat: criando nova tabelas Books

---
+++ back/database/factories/BooksFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Books>
+ */
+class BooksFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(3),
+            'author' => $this->faker->name(),
+            'year' => $this->faker->year(),
+            'isbn' => $this->faker->isbn13()
+        ];
+    }
+}
