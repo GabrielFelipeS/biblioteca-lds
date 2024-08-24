67824e713aee3ed36d51ef6ee8b9157163033bad
Yuri N Bertulucci <yuribertulucci2004@gmail.com>
2024-08-24 19:53:55 -0300

    feat: add belongsTo relationship to book and user class

--- back/app/Models/Reservation.php
+++ back/app/Models/Reservation.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Reservation extends Model
 {
@@ -16,4 +19,14 @@ class Reservation extends Model
         'to',
         'status'
     ];
+
+    public function book(): BelongsTo
+    {
+        return $this->belongsTo(Book::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
