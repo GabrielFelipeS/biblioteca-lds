b78a218a977416a6147b987adc87d8ef96582008
Yuri N Bertulucci <yuribertulucci2004@gmail.com>
2024-08-24 16:54:16 -0300

    feat: add new columns to books table

---
+++ back/database/migrations/2024_08_24_194601_add_new_fields_to_books_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('books', function (Blueprint $table) {
+            $table->string('genre');
+            $table->string('publisher');
+            $table->string('edition');
+            $table->string('image');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('books', function (Blueprint $table) {
+            $table->dropColumn('genre', 'publisher', 'edition', 'image');
+        });
+    }
+};
