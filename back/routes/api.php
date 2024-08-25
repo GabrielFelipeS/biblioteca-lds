bc176df43008284d26f6ace78f101858149cd66a
Yuri N Bertulucci <yuribertulucci2004@gmail.com>
2024-08-25 13:40:39 -0300

    wip: Book api routes

--- back/routes/api.php
+++ back/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,5 +14,10 @@ Route::prefix('auth')->group(function () {
         Route::post('/logout', [AuthController::class, 'logout']);
         Route::get('/validate', [AuthController::class, 'validateToken']);
     });
+});
+
+Route::middleware('auth:api')->group(function () {
+    Route::apiResource('/books', BookController::class)
+        ->withoutMiddleware('auth:api');
 });
 
