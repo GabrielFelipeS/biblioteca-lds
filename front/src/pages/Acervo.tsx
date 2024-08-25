f84744a0acb61f953d8d8468f3ce93fb75c0aeb1
Gabriel Felipe <belebraz16@gmail.com>
2024-08-25 13:49:44 -0300

    feat: Acervo.tsx

---
+++ front/src/pages/Acervo.tsx
@@ -0,0 +1,12 @@
+import {NavBar} from "../components/NavBar.tsx";
+
+export function Acervo() {
+    return (
+        <div className={"bg-ligth-background_secondary h-screen md:h-full"}>
+            <NavBar/>
+            <div className={`h-min w-full flex justify-center pt-14`}>
+                aba
+            </div>
+        </div>
+    )
+}
