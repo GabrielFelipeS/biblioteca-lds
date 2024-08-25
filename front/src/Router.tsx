8b700bc8ec04bf328309912dd84998bdaf206b65
Gabriel Felipe <belebraz16@gmail.com>
2024-08-25 19:56:26 -0300

    feat: new route, emprestimo

--- front/src/Router.tsx
+++ front/src/Router.tsx
@@ -5,6 +5,7 @@ import {Home} from "./pages/Home";
 import {CadastrarLivro} from "./pages/CadastrarLivro";
 import {EditBook} from "./pages/EditBook.tsx";
 import {Acervo} from "./pages/Acervo.tsx";
+import { Emprestimo } from "./pages/Emprestados.tsx";
 
 export function Router() {
     return (
@@ -14,6 +15,7 @@ export function Router() {
                 <Route path={"/login"} element={<Login/>}/>
                 <Route path={"/register"} element={<Register/>}/>
                 <Route path={"/acervo"} element={<Acervo/>}/>
+                <Route path={"/emprestimo"} element={<Emprestimo/>}/>
                 <Route path={"/livro/cadastrar"} element={<CadastrarLivro/>}/>
                 <Route path={"/livro/editar/:id"} element={<EditBook/>}/>
                 <Route path={"/*"} element={<Home/>}/>
