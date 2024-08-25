16c7a19a214754845738742c5f8d5374df1dd877
Micael Hernandes <micaelmacedo100@outlook.com>
2024-08-25 17:00:52 -0300

    feat: criando metodo na camada de serviço para criar reserva

--- back/app/Services/ReservationService.php
+++ back/app/Services/ReservationService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Repositories\ReservationRepository;
+use Illuminate\Support\Facades\Auth;
 
 class ReservationService
 {
@@ -15,5 +16,29 @@ class ReservationService
     public function listByUser($userId)
     {
         return $this->repository->findByUser($userId);
+    }
+    public function create(array $data)
+    {
+        try {
+            $activeReservation = $this->repository->findActiveReservation($data['book_id']);
+            if ($activeReservation) {
+                throw new \Exception('Já existe uma reserva ativa para este livro');
+            }
+            $book = $this->bookService->getBookById($data['book_id']);
+            if (!$book) {
+                throw new \Exception('Livro não encontrado');
+            }
+            $data = [
+                'book_id' => $data['book_id'],
+                'user_id' => Auth::user()->id,
+                'from' => $data['start_date'],
+                'to' => $data['end_date'],
+                'status' => 'pending'
+            ];
+
+            return $this->repository->create($data);
+        } catch (\Throwable $th) {
+            throw new \Exception($th->getMessage());
+        }
     }
 }
