65e3b492b0ddb37b66936598ee9cbf227d00b739
Micael Hernandes <micaelmacedo100@outlook.com>
2024-08-25 20:49:38 -0300

    refactor: ajustando teste para passar token de autenticação de id da reserva

--- back/tests/Feature/Reservations/EditReservationTest.php
+++ back/tests/Feature/Reservations/EditReservationTest.php
@@ -3,8 +3,11 @@
 namespace Tests\Feature\Reservations;
 
 use App\Models\Book;
+use App\Models\Reservation;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Spatie\Permission\Models\Permission;
 use Tests\TestCase;
 
 class EditReservationTest extends TestCase
@@ -12,14 +15,27 @@ class EditReservationTest extends TestCase
     public function test_editando_reserva_com_sucesso()
     {
         $book = Book::factory()->create();
+        $user = User::factory()->create();
+        $token = $user->createToken('token')->accessToken;
+        $permission = Permission::findByName('editar reserva');
+        $user->givePermissionTo($permission);
 
         $newData = [
             'book_id' => $book->id,
-            'start_date' => '2021-10-10',
-            'end_date' => '2021-10-15',
+            'from' => '2021-10-10',
+            'to' => '2021-10-15',
         ];
 
-        $response = $this->putJson('/api/reservation/1', $newData);
+        $reservation = Reservation::factory()->create([
+            'book_id' => $book->id,
+            'user_id' => $user->id,
+            'status' => 'pending',
+        ]);
+
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->putJson('/api/reservation/' . $reservation->id, $newData);
 
         $response->assertStatus(200);
     }
