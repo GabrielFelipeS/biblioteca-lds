5215472c728c6d5c8b5d7a50faadaab2d481953d
Micael Hernandes <micaelmacedo100@outlook.com>
2024-08-25 17:51:51 -0300

    test: tentativa de criação de reserva com livro ja reservado

--- back/tests/Feature/Reservations/CreateReservationTest.php
+++ back/tests/Feature/Reservations/CreateReservationTest.php
@@ -29,4 +29,30 @@ class CreateReservationTest extends TestCase
 
         $response->assertStatus(201);
     }
+
+    public function test_teste_de_criacao_com_livro_ja_reservado()
+    {
+        $user = \App\Models\User::factory()->create();
+        $permission =  Permission::findByName('criar reserva');
+        $user->givePermissionTo($permission);
+        $token = $user->createToken('token')->accessToken;
+        $book = \App\Models\Book::factory()->create();
+        $reservation = \App\Models\Reservation::factory()->create([
+            'book_id' => $book->id,
+            'from' => '2021-10-10',
+            'to' => '2021-10-20',
+        ]);
+
+        $data = [
+            'book_id' => $book->id,
+            'from' => '2021-10-10',
+            'to' => '2021-10-20',
+        ];
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->postJson('/api/reservation', $data);
+
+        $response->assertStatus(422);
+    }
 }
