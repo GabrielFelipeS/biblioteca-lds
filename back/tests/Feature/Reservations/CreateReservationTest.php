2c2ac3a1c6face7d1e571dc3d4a0807a77fed1d0
Micael Hernandes <micaelmacedo100@outlook.com>
2024-08-25 15:04:12 -0300

    test: criando teste de nova reserva

---
+++ back/tests/Feature/Reservations/CreateReservationTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature\Reservations;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Spatie\Permission\Models\Permission;
+use Tests\TestCase;
+
+class CreateReservationTest extends TestCase
+{
+    public function test_de_criacao_de_reserva_com_dados_validos()
+    {
+        $user = \App\Models\User::factory()->create();
+        $permission =  Permission::findByName('criar reserva');
+        $user->givePermissionTo($permission);
+        $token = $user->createToken('token')->accessToken;
+        $book = \App\Models\Book::factory()->create();
+
+        $data = [
+            'book_id' => $book->id,
+            'start_date' => '2021-10-10',
+            'end_date' => '2021-10-20',
+        ];
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->postJson('/api/reservation', $data);
+
+        $response->assertStatus(201);
+    }
+}
