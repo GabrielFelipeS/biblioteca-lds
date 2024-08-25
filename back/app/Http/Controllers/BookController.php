fd62b4e9e28df70be0d0bb9a02bc1edcee9cc942
Yuri N Bertulucci <yuribertulucci2004@gmail.com>
2024-08-25 14:23:34 -0300

    refactor: change methods to use the BookRepository

--- back/app/Http/Controllers/BookController.php
+++ back/app/Http/Controllers/BookController.php
@@ -3,16 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Models\Book;
+use App\Services\BookService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
+
+    function __construct(private BookService $service)
+    {
+
+    }
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): JsonResponse
     {
-        return Book::all()->jsonSerialize();
+        $books = $this->service->getAllBooks();
+        return response()->json($books, 200);
     }
 
     /**
@@ -26,9 +34,10 @@ class BookController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(string $id): JsonResponse
     {
-        return Book::findOrFail($id);
+        $book = $this->service->getBookById($id);
+        return response()->json($book, 200);
     }
 
     /**
@@ -42,9 +51,10 @@ class BookController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(string $id): JsonResponse
     {
-        Book::destroy($id);
-        return response()->json(['message' => 'Livro removido com sucesso'], 200);
+        $book = $this->service->getBookById($id);
+        $this->service->deleteBook($id);
+        return response()->json(['message' => 'Livro \'' . $book->title . '\' removido com sucesso'], 200);
     }
 }
