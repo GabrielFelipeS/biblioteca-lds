b048189996c4fba7330811ab69da6234ec3c834b
Gabriel Felipe <belebraz16@gmail.com>
2024-08-25 15:09:27 -0300

    fix: CORS

--- front/src/services/api.ts
+++ front/src/services/api.ts
@@ -2,9 +2,11 @@ import axios from "axios"
 
 export const url = "http://localhost:8000/api/" as string;
 
+const config = {
+    headers: {'Content-Type': 'application/json'}
+};
+
 export const api = axios.create({
     baseURL: url,
-    headers: {
-        'Content-Type': 'application/json'
-    },
+    config
 });
