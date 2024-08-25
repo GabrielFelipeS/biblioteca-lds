0bdc3091435e355451a0781e8877f61d385745c7
Micael Hernandes <micaelmacedo100@outlook.com>
2024-08-25 17:00:34 -0300

    feat: criando metodo para registrar reserva

--- back/app/Http/Controllers/ReservationController.php
+++ back/app/Http/Controllers/ReservationController.php
@@ -22,4 +22,17 @@ class ReservationController extends Controller
             return response()->json(['message' => 'Error'], 500);
         }
     }
+
+    public function store(Request $request)
+    {
+        try {
+            Log::info('Recebida requisição para criar reserva do usuário: ' . $request->user()->id);
+            $reservation = $this->service->create($request->all());
+            Log::info('Reserva criada com sucesso');
+            return response()->json($reservation, 201);
+        } catch (\Throwable $th) {
+            Log::error('Erro ao criar reserva: ' . $th->getMessage());
+            return response()->json(['message' => 'Error'], 500);
+        }
+    }
 }
