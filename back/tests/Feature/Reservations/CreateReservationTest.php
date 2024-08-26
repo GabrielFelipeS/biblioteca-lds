7d7567343773a48848971e276c67092af3b02652
Micael Hernandes <micaelmacedo100@outlook.com>
2024-08-25 22:59:29 -0300

    refactor: alterando testes de reserva para datas do dia seguinte e +10 na devolução dos livros

--- back/tests/Feature/Reservations/CreateReservationTest.php
+++ back/tests/Feature/Reservations/CreateReservationTest.php
@@ -19,8 +19,8 @@ class CreateReservationTest extends TestCase
 
         $data = [
             'book_id' => $book->id,
-            'from' => '2021-10-10',
-            'to' => '2021-10-20',
+            'from' => Date('Y-m-d', strtotime('+1 day')),
+            'to' => Date('Y-m-d', strtotime('+10 days')),
         ];
 
         $response = $this->withHeaders([
@@ -31,8 +31,8 @@ class CreateReservationTest extends TestCase
 
         $this->assertDatabaseHas('reservations', [
             'book_id' => $book->id,
-            'from' => '2021-10-10',
-            'to' => '2021-10-20',
+            'from' => Date('Y-m-d', strtotime('+1 day')),
+            'to' => Date('Y-m-d', strtotime('+10 days')),
             'status' => 'pending'
         ]);
     }
@@ -46,14 +46,14 @@ class CreateReservationTest extends TestCase
         $book = \App\Models\Book::factory()->create();
         $reservation = \App\Models\Reservation::factory()->create([
             'book_id' => $book->id,
-            'from' => '2021-10-10',
-            'to' => '2021-10-20',
+            'from' => Date('Y-m-d', strtotime('+1 day')),
+            'to' => Date('Y-m-d', strtotime('+10 days')),
         ]);
 
         $data = [
             'book_id' => $book->id,
-            'from' => '2021-10-10',
-            'to' => '2021-10-20',
+            'from' => Date('Y-m-d', strtotime('+1 day')),
+            'to' => Date('Y-m-d', strtotime('+10 days')),
         ];
 
         $response = $this->withHeaders([
@@ -65,7 +65,8 @@ class CreateReservationTest extends TestCase
         $this->assertEquals('Livro já reservado', $response['message']);
     }
 
-    public function test_tentativa_de_nova_reserva_ja_tendo_3_livros_reservados(){
+    public function test_tentativa_de_nova_reserva_ja_tendo_3_livros_reservados()
+    {
         $user = \App\Models\User::factory()->create();
         $permission =  Permission::findByName('criar reserva');
         $user->givePermissionTo($permission);
@@ -78,31 +79,31 @@ class CreateReservationTest extends TestCase
         $reservation = \App\Models\Reservation::factory()->create([
             'user_id' => $user->id,
             'book_id' => $book->id,
-            'from' => '2021-10-10',
-            'to' => '2021-10-20',
+            'from' => Date('Y-m-d', strtotime('+1 day')),
+            'to' => Date('Y-m-d', strtotime('+10 days')),
             'status' => 'pending'
         ]);
 
         $reservation2 = \App\Models\Reservation::factory()->create([
             'user_id' => $user->id,
             'book_id' => $book2->id,
-            'from' => '2021-10-10',
-            'to' => '2021-10-20',
+            'from' => Date('Y-m-d', strtotime('+1 day')),
+            'to' => Date('Y-m-d', strtotime('+10 days')),
             'status' => 'pending'
         ]);
 
         $reservation3 = \App\Models\Reservation::factory()->create([
             'user_id' => $user->id,
             'book_id' => $book3->id,
-            'from' => '2021-10-10',
-            'to' => '2021-10-20',
+            'from' => Date('Y-m-d', strtotime('+1 day')),
+            'to' => Date('Y-m-d', strtotime('+10 days')),
             'status' => 'pending'
         ]);
 
         $data = [
             'book_id' => $book4->id,
-            'from' => '2021-10-10',
-            'to' => '2021-10-20',
+            'from' => Date('Y-m-d', strtotime('+1 day')),
+            'to' => Date('Y-m-d', strtotime('+10 days')),
         ];
 
         $response = $this->withHeaders([
@@ -112,10 +113,10 @@ class CreateReservationTest extends TestCase
         $response->assertStatus(422);
 
         $this->assertEquals('Usuário já possui 3 reservas ativas', $response['message']);
-
     }
 
-    public function test_tentativa_de_criacao_de_reserva_com_livro_nao_encontrado(){
+    public function test_tentativa_de_criacao_de_reserva_com_livro_nao_encontrado()
+    {
         $user = \App\Models\User::factory()->create();
         $permission =  Permission::findByName('criar reserva');
         $user->givePermissionTo($permission);
@@ -123,16 +124,16 @@ class CreateReservationTest extends TestCase
 
         $data = [
             'book_id' => 10000,
-            'from' => '2021-10-10',
-            'to' => '2021-10-20',
+            'from' => Date('Y-m-d', strtotime('+1 day')),
+            'to' => Date('Y-m-d', strtotime('+10 days')),
         ];
 
         $response = $this->withHeaders([
             'Authorization' => 'Bearer ' . $token,
         ])->postJson('/api/reservation', $data);
 
-        $response->assertStatus(404);
+        $response->assertStatus(422);
 
-        $this->assertEquals('Livro não encontrado', $response['message']);
+        $this->assertEquals('O livro informado não existe', $response['errors']['book_id'][0]);
     }
 }
