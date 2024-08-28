f0a8960176b0c4cba1bfa9bf40eda6ca5397bc71
Gabriel Felipe <belebraz16@gmail.com>
2024-08-28 14:06:25 -0300

    feat: th with API books

--- front/src/pages/Emprestimo.tsx
+++ front/src/pages/Emprestimo.tsx
@@ -35,36 +35,36 @@ export function Emprestimo() {
                 <table className={"relative overflow-x-auto"}>
                     <thead>
                         <tr className="text-ligth-primary bg-black">
+                            <th className="px-9 py-3 "></th>
                             <th className="px-9 py-3"></th>
-                            <th className="px-9 py-3"></th>
-                            <th className="px-6 py-3">Titulo</th>
-                            <th className="px-6 py-3">Autor</th>
-                            <th className="px-6 py-3 hidden lg:table-cell">Gênero</th>
-                            <th className="px-6 py-3 hidden lg:table-cell">ISBN</th>
-                            <th className="px-6 py-3 hidden lg:table-cell">Lançamento</th>
-                            <th className="px-6 py-3 hidden lg:table-cell">Editora</th>
-                            <th className="px-6 py-3 hidden lg:table-cell">Edição</th>
-                            <th className="px-6 py-3">Unidades</th>
+                            <th className="px-6 py-3 border">Titulo</th>
+                            <th className="px-6 py-3 border">Autor</th>
+                            <th className="px-6 py-3 border hidden lg:table-cell">Gênero</th>
+                            <th className="px-6 py-3 border hidden lg:table-cell">ISBN</th>
+                            <th className="px-6 py-3 border hidden lg:table-cell">Lançamento</th>
+                            <th className="px-6 py-3 border hidden lg:table-cell">Editora</th>
+                            <th className="px-6 py-3 border hidden lg:table-cell">Edição</th>
+                            <th className="px-6 py-3 border">Unidades</th>
                         </tr>
                     </thead>
                     <tbody>
                        { books && books.map((book, index) => {
                             return (
                                 <tr className="h-12" key={index}> 
-                                    <td className="px-2 cursor-pointer">
+                                    <td className="px-2 cursor-pointer border">
                                         <div className="bg-ligth-tertiary text-ligth-primary p-1 rounded-lg">Renovação</div>
                                     </td>
-                                    <td className="px-2 cursor-pointer">
+                                    <td className="px-2 cursor-pointer border">
                                         <div className="bg-ligth-tertiary text-ligth-primary p-1 rounded-lg">Devolução </div>
                                     </td>
-                                    <td className="px-6">Exemplo</td>
-                                    <td className="px-6">Autor</td>
-                                    <td className="px-6 hidden lg:table-cell">Gênero</td>
-                                    <td className="px-6 hidden lg:table-cell">ISBN</td>
-                                    <td className="px-6 hidden lg:table-cell">Lançamento</td>
-                                    <td className="px-6 hidden lg:table-cell">Editora</td>
-                                    <td className="px-6 hidden lg:table-cell">Edição</td>
-                                    <td className="px-6">5/10</td>
+                                    <td className="px-6 border">{book.title}</td>
+                                    <td className="px-6 border">{book.author}</td>
+                                    <td className="px-6 border hidden lg:table-cell">{book.genre}</td>
+                                    <td className="px-6 border hidden lg:table-cell">{book.isbn}</td>
+                                    <td className="px-6 border hidden lg:table-cell">{book.year}</td>
+                                    <td className="px-6 border hidden lg:table-cell">{book.publisher}</td>
+                                    <td className="px-6 border hidden lg:table-cell">{book.edition}</td>
+                                    <td className="px-6 border">5/10</td>
                                 </tr>
                         )})}
                     </tbody>
