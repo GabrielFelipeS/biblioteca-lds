44f451470c11b53ecc2cd8a511cda0d8e7b5bbdc
Gabriel Felipe <belebraz16@gmail.com>
2024-08-28 13:17:12 -0300

    fix: Router with path '/', redirect to /home

--- front/src/Router.tsx
+++ front/src/Router.tsx
@@ -1,20 +1,22 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import {Login} from "./pages/Login";
 import {Register} from "./pages/Register";
 import {Home} from "./pages/Home";
 import {CadastrarLivro} from "./pages/CadastrarLivro";
 import {EditBook} from "./pages/EditBook.tsx";
+import { Emprestimo } from "./pages/Emprestimo";
 
 export function Router() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path={"/"} element={<Home/>}/>
                 <Route path={"/home"} element={<Home/>}/>
                 <Route path={"/login"} element={<Login/>}/>
                 <Route path={"/register"} element={<Register/>}/>
+                <Route path={"/emprestimo"} element={<Emprestimo/>}/>
                 <Route path={"/livro/cadastrar"} element={<CadastrarLivro/>}/>
                 <Route path={"/livro/editar/:id"} element={<EditBook/>}/>
+                <Route path="/*" element={<Navigate to="/home" replace />} />
             </Routes>
         </BrowserRouter>
     )
