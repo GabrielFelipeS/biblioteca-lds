275627bdbcabba5c95db14995051c663c29471ba
Micael Hernandes <micaelmacedo100@outlook.com>
2024-08-28 23:00:41 -0300

    feat: ajustando test com credenciais de bibliotecario

--- back/tests/Feature/Reservations/UpdateReservationStatusTest.php
+++ back/tests/Feature/Reservations/UpdateReservationStatusTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Reservations;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -10,14 +11,17 @@ class UpdateReservationStatusTest extends TestCase
 {
     public function test_liberando_reserva_com_sucesso()
     {
-        $response = $this->putJson('/api/reservation/1', [
-            'status' => 'approved'
-        ]);
+        $user = User::all()->where('name', 'Biblioteca IFSP')->first();
+
+        $token = $user->createToken('token')->accessToken;
+
+        $response = $this->withHeader('Authorization', 'Bearer ' . $token)
+            ->putJson('/api/reservation/1', ['status' => 'canceled']);
 
         $response->assertStatus(200);
 
         $response->assertJson([
-            'message' => 'Reserva Liberada com sucesso'
+            'message' => 'Status da reserva alterado com sucesso'
         ]);
     }
 }
