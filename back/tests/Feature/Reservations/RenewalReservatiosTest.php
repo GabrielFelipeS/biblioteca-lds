479e1bcb31e43f8b38b693544af47e51bc8bacf4
Micael Hernandes <micaelmacedo100@outlook.com>
2024-08-28 23:47:10 -0300

    test: tentativa de renovação com reserva com status imutavel

--- back/tests/Feature/Reservations/RenewalReservatiosTest.php
+++ back/tests/Feature/Reservations/RenewalReservatiosTest.php
@@ -43,4 +43,37 @@ class RenewalReservatiosTest extends TestCase
             'status' => 'approved',
         ]);
     }
+
+    public function test_renovacao_de_reserva_com_status_diferente_de_aprovado()
+    {
+        $user = User::all()->where('name', 'Biblioteca IFSP')->first();
+
+        $token = $user->createToken('token')->accessToken;
+
+        $reservation = Reservation::factory()->create([
+            'to' => Date('Y-m-d', strtotime('-8 day')),
+            'from' => Date('Y-m-d', strtotime('-1 day')),
+            'status' => 'pending',
+        ]);
+
+        $data = [
+            'to' => Date('Y-m-d', strtotime('+7 day')),
+        ];
+
+        $response = $this->withHeader('Authorization', 'Bearer ' . $token)
+            ->putJson('/api/reservation/' . $reservation->id . '/renewal', $data);
+
+        $response->assertStatus(422);
+
+        $response->assertJson([
+            'message' => 'Reserva pendente'
+        ]);
+
+        $this->assertDatabaseHas('reservations', [
+            'id' => $reservation->id,
+            'to' => Date('Y-m-d', strtotime('-8 day')),
+            'from' => Date('Y-m-d', strtotime('-1 day')),
+            'status' => 'pending',
+        ]);
+    }
 }
