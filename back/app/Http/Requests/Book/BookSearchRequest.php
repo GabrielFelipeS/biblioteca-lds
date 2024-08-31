1f4d3bb0fd8b44b596b5d8e561f8a692b9433432
Internautilo <yuribertulucci2004@gmail.com>
2024-08-30 21:19:49 -0300

    feat: add BookSearchRequest

---
+++ back/app/Http/Requests/Book/BookSearchRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests\Book;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class BookSearchRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => ['string', 'sometimes'],
+            'author' => ['string', 'sometimes'],
+            'genre' => ['string', 'sometimes'],
+            'year' => ['string', 'sometimes'],
+            'isbn' => ['string', 'sometimes'],
+            'publisher' => ['string', 'sometimes'],
+            'edition' => ['string', 'sometimes'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'title.string' => 'O título deve ser uma string.',
+            'author.string' => 'O autor deve ser uma string.',
+            'genre.string' => 'O gênero deve ser uma string.',
+            'year.string' => 'O ano deve ser uma string.',
+            'isbn.string' => 'O ISBN deve ser uma string.',
+            'publisher.string' => 'A editora deve ser uma string.',
+            'edition.string' => 'A edição deve ser uma string.',
+        ];
+    }
+}
