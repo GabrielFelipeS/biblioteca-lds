5a831c2e139a9f8c63043474abedc574324f933d
Gabriel Felipe <belebraz16@gmail.com>
2024-08-31 16:36:17 -0300

    feat: reservar

--- front/src/types/Reservation.ts
+++ front/src/types/Reservation.ts
@@ -2,45 +2,45 @@ type Status = 'pending' | 'approved' | 'returned' | 'canceled' | 'expired' | 'ov
 
 export type Reservation = {
     id: number
-    book_id: number
+    book_id: string
     user_id: number
-    from: Date
-    to: Date
+    from: string
+    to: string
     status: Status
 }
 
-const emptyReservation: Reservation = {
+export const emptyReservation: Reservation = {
     id: 0,
-    book_id: 0,
+    book_id: '0',
     user_id: 0,
-    from: new Date(),
-    to: new Date(),
+    from: '2024-09-01',
+    to: '2024-09-08',
     status: 'pending'
 };
 
 export const reservationsMock: Reservation[] = [
     {
         id: 1,
-        book_id: 1,
+        book_id: '1',
         user_id: 201,
-        from: new Date('2024-09-01'),
-        to: new Date('2024-09-10'),
+        from: '2024-09-01',
+        to: '2024-09-10',
         status: 'approved'
     },
     {
         id: 2,
-        book_id: 2,
+        book_id: '2',
         user_id: 202,
-        from: new Date('2024-09-05'),
-        to: new Date('2024-09-15'),
+        from: '2024-09-05',
+        to: '2024-09-15',
         status: 'pending'
     },
     {
         id: 3,
-        book_id: 3,
+        book_id: '3',
         user_id: 203,
-        from: new Date('2024-09-10'),
-        to: new Date('2024-09-20'),
+        from: '2024-09-10',
+        to: '2024-09-20',
         status: 'canceled'
     }
 ];
