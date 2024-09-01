edfe7738164aa62c091772efd257cf3a5d8626d2
Gabriel Felipe <belebraz16@gmail.com>
2024-09-01 00:39:52 -0300

    fix: padding bottom

--- front/src/pages/FichaTecnica.tsx
+++ front/src/pages/FichaTecnica.tsx
@@ -34,22 +34,25 @@ export function FichaTecnica() {
             <BackArrow/>
             <div className="flex justify-center pt-14">
                 <div className={`h-full w-full flex flex-col justify-center items-center`}>
-                    <div className="w-2/4 h-min bg-white mt-9 pb-20 text-ligth-button drop-shadow-lg">
+                    <div className="w-2/4 h-min bg-white mt-9 pb-4 sm:pb-20 text-ligth-button drop-shadow-lg">
                         <p className="ml-4 mt-1 text-center font-bold text-lg">Ficha tecnica</p>
                         <div className="mt-5 ml-2 mr-2 flex flex-col items-center md:flex-row">
-                            <img src={image} alt="imagem do livro" className="w-48 h-72 mr-5"/>
-                            <div className="bg-ligth-background_secondary mb-3 w-2/3">
-                                <p className="mb-3"><strong>{book.title}</strong></p>
+
+                            <img src={image} alt="imagem do livro" className="w-28 h-44 sm:w-48 sm:h-72 sm:mr-5 mb-3 sm:mb-0"/>
+
+                            <div className="bg-ligth-background_secondary mb-3 w-5/6 sm:w-2/3 p-2">
+                                <p className="mb-3 text-center"><strong>{book.title}</strong></p>
+
                                 <p>Código ISBN: {book.isbn}</p>
                                 <p>Autor: {book.author}</p>
                                 <p>Edição: {book.edition}</p>
                                 <p>Gênero: {book.genre}</p>
                                 <p>Lançamento: {book.year}</p>
+
                                 <div className="mt-6 mb-5 flex justify-center">
-                                    <button className="rounded-2xl px-9 py-3 bg-ligth-dark_gray text-ligth-ligth_gray">Reservar</button>
+                                    <button className="rounded-2xl px-5 sm:px-9 py-3 bg-ligth-dark_gray text-ligth-ligth_gray">Reservar</button>
                                 </div>
                             </div>
-    
                         </div>
                     </div>
                 </div>
