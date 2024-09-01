29ea247e0d08ebd98c07afa9df84348b29a03bf4
Micael Hernandes <micaelmacedo100@outlook.com>
2024-09-01 12:12:06 -0300

    test: buscando reservas com status pendentes

--- back/tests/Feature/Admin/ShowReservationsTest.php
+++ back/tests/Feature/Admin/ShowReservationsTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Admin;
 
+use App\Models\Reservation;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -21,5 +22,27 @@ class ShowReservationsTest extends TestCase
         ])->getJson('/api/admin/reservations');
 
         $response->assertStatus(200);
+    }
+
+    public function test_buscando_reservas_com_status_pendente()
+    {
+        $user = User::find(1);
+        $token = $user->createToken('token')->accessToken;
+
+        $allReservations = Reservation::where('status', 'pending')->count();
+
+        $reservations = Reservation::factory()->count(5)->create([
+            'status' => 'pending'
+        ]);
+
+        $response = $this->withHeaders([
+            "Authorization" => "Bearer $token",
+        ])->getJson('/api/admin/reservations?status=pending');
+
+        $response->assertStatus(200);
+
+        $size = $allReservations+5;
+
+        $response->assertJsonCount($size);
     }
 }
