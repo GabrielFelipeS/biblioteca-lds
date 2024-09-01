f22e15e1b21a926185c88034875e717ed2b9e99b
Gabriel Felipe <belebraz16@gmail.com>
2024-08-31 21:26:43 -0300

    feat: ficha tecnica

--- front/src/pages/FichaTecnica.tsx
+++ front/src/pages/FichaTecnica.tsx
@@ -9,7 +9,7 @@ export function FichaTecnica() {
     const [book, setBook] = useState<Book>(LivroEmpty)
     const bearer = "Bearer " + localStorage.getItem("token");
 
-    const image = book.image.includes("storage") ? dominio + book.image :  book.image
+    const image = book.image.includes("storage") ? dominio + book.image : book.image
 
     console.log("aqui" + document.referrer)
 
@@ -29,20 +29,29 @@ export function FichaTecnica() {
 
     return (
         <div className={"bg-ligth-background_secondary pb-20 h-full w-full"}>
-        <NavBar />
-       
-        <div className="flex justify-center pt-14">
-            <div className={`h-full w-full flex flex-col justify-center items-center`}>
-  
-                <div className="w-2/5 bg-white mt-9 text-ligth-button h-40 drop-shadow-lg">
-                    <p className="ml-4 mt-1">Ficha tecnica</p>
-                    <div>
-                        <img src={image} alt="" />
+            <NavBar />
+            <div className="flex justify-center pt-14">
+                <div className={`h-full w-full flex flex-col justify-center items-center`}>
+                    <div className="w-2/4 h-min bg-white mt-9 pb-20 text-ligth-button drop-shadow-lg">
+                        <p className="ml-4 mt-1 text-center font-bold text-lg">Ficha tecnica</p>
+                        <div className="mt-5 ml-2 mr-2 flex flex-col items-center md:flex-row">
+                            <img src={image} alt="imagem do livro" className="w-48 h-72 mr-5"/>
+                            <div className="bg-ligth-background_secondary mb-3 w-2/3">
+                                <p className="mb-3"><strong>{book.title}</strong></p>
+                                <p>Código ISBN: {book.isbn}</p>
+                                <p>Autor: {book.author}</p>
+                                <p>Edição: {book.edition}</p>
+                                <p>Gênero: {book.genre}</p>
+                                <p>Lançamento: {book.year}</p>
+                                <div className="mt-6 mb-5 flex justify-center">
+                                    <button className="rounded-2xl px-9 py-3 bg-ligth-dark_gray text-ligth-ligth_gray">Reservar</button>
+                                </div>
+                            </div>
+    
+                        </div>
                     </div>
                 </div>
-
             </div>
         </div>
-    </div>
     )
 }
