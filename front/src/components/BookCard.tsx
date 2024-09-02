d1fa07c8455cccf59611013806663df643b3b880
Gabriel Felipe <belebraz16@gmail.com>
2024-09-02 14:17:25 -0300

    feat: alert de confirmação de renovação

--- front/src/components/BookCard.tsx
+++ front/src/components/BookCard.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import { Reservation } from "../types/Reservation"
 import { LivroEmpty } from "../types/Book"
 import { api, dominio } from "../services/api";
+import Swal from "sweetalert2";
 
 interface BookCardProps {
     reservation: Reservation
@@ -26,6 +27,43 @@ export function BookCard({reservation}: BookCardProps) {
         }).catch(e => console.log(e))    
     })
 
+    function handleRenewal(id: number) {
+        const date = new Date()
+        date.setDate(date.getDate() + 6)
+
+        Swal.fire({
+            title: "Você tem certeza?",
+            text: `Você não vai poder reverter o processo!`,
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Sim, renovar o livro!",
+            cancelButtonText: "Cancelar"
+        }).then(response => {
+            if (response.isConfirmed) {
+                api.put(`reservation/${id}/renewal`,
+                    {
+                        to: date
+                    },
+                    {
+                        headers: {
+                            Accept: 'application/json',
+                            'Content-Type': 'application/json',
+                            Authorization: bearer
+                        }
+                    }).then( () => {
+                        Swal.fire({
+                            title: "Renovado!",
+                            text: "Livro renovado com sucesso.",
+                            icon: "success"
+                        });
+                    })
+                    .catch(e => console.log(e))
+            }
+        })
+    }
+
     return (
         <div className="w-1/3 rounded overflow-hidden shadow-lg bg-white relative group">
             <img className="w-full" src={image} alt="Capa do Livro" />
@@ -35,7 +73,7 @@ export function BookCard({reservation}: BookCardProps) {
             </div>
 
             <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                <button className="bg-ligth-background text-white font-bold py-2 px-4 rounded">Renovar</button>
+                <button onClick={() => handleRenewal(reservation.id)} className="bg-ligth-background text-white font-bold py-2 px-4 rounded">Renovar</button>
             </div>
         </div>
     )
