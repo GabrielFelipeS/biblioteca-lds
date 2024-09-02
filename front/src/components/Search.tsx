553fb478d3d6f8594f36286d0904558b5501ce2c
Gabriel Felipe <belebraz16@gmail.com>
2024-09-02 15:47:46 -0300

    feat: search with debouce

--- front/src/components/Search.tsx
+++ front/src/components/Search.tsx
@@ -1,20 +1,40 @@
-import { useEffect, useState } from "react"
-import lupa from "../assets/lupa.png"
-import { Book, LivroEmpty } from "../types/Book"
-
+import { useCallback, useEffect, useState } from "react";
+import lupa from "../assets/lupa.png";
+import { api } from "../services/api";
+import { Book } from "../types/Book";
+import {debounce} from 'lodash';
 
 interface SearchProps {
     setBooks: React.Dispatch<React.SetStateAction<Book[]>>
 }
 
 export function Search({ setBooks }: SearchProps) {
-    const [search, setSearch] = useState<string>();
+    const [search, setSearch] = useState<string>('');
+    const bearer = "Bearer " + localStorage.getItem("token");
+    
+
+    const fetchBooks  = useCallback(
+        debounce(async (query: string) => {
+            api.get(`books/search/${query}`,
+                {
+                    headers: {
+                        Accept: 'application/json',
+                        'Content-Type': 'application/json',
+                        Authorization: bearer
+                    }
+                })
+                .then(response => {
+                    console.log(response)
+                    setBooks(response.data)
+                })
+        }, 750), // 500ms de atraso
+        []
+      );
 
     useEffect(() => {
-        // LOGICA DE BUSCA NA API AQUI
-
-        // IGNORA ESSA LINHA
-        setBooks([LivroEmpty, LivroEmpty])
+      if(search) {
+        fetchBooks(search);
+      }
     }, [search])
 
 
